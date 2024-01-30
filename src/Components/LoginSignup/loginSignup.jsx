23d97b75443f0fdc720e6143eff0033df04e84f2
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-01-31 00:37:20 +0100

    first commiy

---
+++ src/Components/LoginSignup/loginSignup.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './loginSignup.css'
+
+import user_icon from '../Assets/user.png'
+import email_icon from '../Assets/email.png'
+import passwprd_icon from '../Assets/padlock.png'
+
+const loginSignup = () => {
+    return ( 
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" />
+                </div>
+                <div className="input">
+                    <img src={passwprd_icon} alt="" />
+                    <input type="password" />
+                </div>
+            </div>
+
+        </div>
+     );
+}
+ 
+export default loginSignup;
