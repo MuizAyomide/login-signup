b1c896f30956013b6079cbd50b2794bd911e1acb
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-01-31 01:11:07 +0100

    style

--- src/Components/LoginSignup/loginSignup.jsx
+++ src/Components/LoginSignup/loginSignup.jsx
@@ -15,18 +15,18 @@ const loginSignup = () => {
             <div className="inputs">
                 <div className="input">
                     <img src={user_icon} alt="" />
-                    <input type="text" />
+                    <input type="text" placeholder='Name'/>
                 </div>
                 <div className="input">
                     <img src={email_icon} alt="" />
-                    <input type="email" />
+                    <input type="email" placeholder='Email'/>
                 </div>
                 <div className="input">
                     <img src={passwprd_icon} alt="" />
-                    <input type="password" />
+                    <input type="password" placeholder='Password'/>
                 </div>
             </div>
-            <div className="forgot-password">Lost Password <span>Click Here!</span></div>
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
             <div className="submit-container">
                 <div className="submit">Sign Up</div>
                 <div className="submit">Login</div>
