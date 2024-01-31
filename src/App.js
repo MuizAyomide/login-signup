f40eef68ee2a7464f5c04ab455b513f2f7342956
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-01-31 13:03:02 +0100

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-import LoginSignup from './Components/loginSignup/LoginSignup';
+import LoginSignup from './Components/loginSignup/LoginSignup.jsx';
 function App() {
   return (
     <div >
--- src/Components/LoginSignup/loginSignup.jsx
+++ src/Components/LoginSignup/loginSignup.jsx
@@ -1,22 +1,26 @@
-// import React from "react";
+import react, { useState } from 'react';
 import './loginSignup.css'
 
 import user_icon from '../Assets/user.png'
 import email_icon from '../Assets/email.png'
 import passwprd_icon from '../Assets/padlock.png'
 
-const loginSignup = () => {
+const LoginSignup = () => {
+
+const [action,setAction] = useState("Login");
+
     return ( 
         <div className="container">
             <div className="header">
-                <div className="text">Sign Up</div>
+                <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
-                <div className="input">
+                {action === "Login"?<div></div>:<div className="input">
                     <img src={user_icon} alt="" />
                     <input type="text" placeholder='Name'/>
-                </div>
+                </div>}
+                
                 <div className="input">
                     <img src={email_icon} alt="" />
                     <input type="email" placeholder='Email'/>
@@ -26,10 +30,10 @@ const loginSignup = () => {
                     <input type="password" placeholder='Password'/>
                 </div>
             </div>
-            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+            {action === "Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action==="Login"?"submit gray":"submit"} onClick={ () => {setAction("Sign Up")}} >Sign Up</div>
+                <div className={action==="Sign Up"?"submit gray":"submit"} onClick={ () => {setAction("Login")}} >Login</div>
 
             </div>
 
@@ -37,4 +41,4 @@ const loginSignup = () => {
      );
 }
  
-export default loginSignup;
+export default LoginSignup;
